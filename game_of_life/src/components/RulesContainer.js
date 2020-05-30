0fd23356f8fc2dbaf80d4f4316176ccd43911a20
unknown <viola4lfe@gmail.com>
2020-05-30 11:23:30 -0400

    portfolio updates

--- game_of_life/src/components/RulesContainer.js
+++ game_of_life/src/components/RulesContainer.js
@@ -9,9 +9,12 @@ export default function RulesContainer() {
       <p>
         The universe of the Game of Life is an infinite, two-dimensional
         orthogonal grid of square cells, each of which is in one of two possible
-        states, alive or dead, (or populated and unpopulated, respectively).
+        states, alive or dead.
+        <br />
         Every cell interacts with its eight neighbors, which are the cells that
-        are horizontally, vertically, or diagonally adjacent. At each step in
+        are horizontally, vertically, or diagonally adjacent.
+        <br /> 
+        At each step in
         time, the following transitions occur:
       </p>
       <ol>
@@ -32,13 +35,12 @@ export default function RulesContainer() {
           reproduction.
         </li>
       </ol>
-      <p>
-        The initial pattern constitutes the seed of the system. The first
+      <p> 
+        The first
         generation is created by applying the above rules simultaneously to
         every cell in the seed; births and deaths occur simultaneously, and the
         discrete moment at which this happens is sometimes called a tick. Each
-        generation is a pure function of the preceding one. The rules continue
-        to be applied repeatedly to create further generations.
+        generation is a pure function of the preceding one.
       </p>
     </div>
   );
